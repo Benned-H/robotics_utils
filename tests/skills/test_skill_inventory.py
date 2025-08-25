abf3b5aec62ce38b88c22070cf3ab53438e268e6
Benned Hedegaard <benned1@gmail.com>
2025-08-25 17:44:09 -0400

    Apply suggestions from code review

--- tests/skills/test_skill_inventory.py
+++ tests/skills/test_skill_inventory.py
@@ -45,7 +45,7 @@ def test_skills_inventory_to_from_yaml_data(inventory: SkillsInventory) -> None:
 
 def test_skills_inventory_from_protocol(example_skills_protocol: SkillsProtocol) -> None:
     """Verify that a SkillsInventory is correctly constructed from an example Python protocol."""
-    # Arrange/Act - An example skills protocol is provided via a pytest fixture
+    # Arrange/Act - An example skills protocol is provided by a pytest fixture
     inventory = SkillsInventory.from_protocol(example_skills_protocol)
 
     # Assert - Verify that the constructed SkillsInventory contains the expected skills
@@ -62,7 +62,7 @@ def test_skills_inventory_from_protocol(example_skills_protocol: SkillsProtocol)
 
 
 def test_skill_execution(example_skills_protocol: SkillsProtocol) -> None:
-    """Verify that skills constructed from a skills protocol can be correctly executed."""
+    """Verify that skills constructed from a skills protocol can be executed."""
     # Arrange - Convert the skills protocol into an inventory of skills
     inventory = SkillsInventory.from_protocol(example_skills_protocol)
 
@@ -106,15 +106,15 @@ def test_skill_instance_from_string(
     # Act - Convert the example strings into SkillInstances
     results = [SkillInstance.from_string(s, inventory, example_objects) for s in example_strings]
 
-    # Assert - Expect that the constructed SkillInstances extracted the correct bindings
+    # Assert - Expect that the constructed SkillInstances have correct bindings
     return_true_instance, forty_four_instance, negative_five_instance = results
     assert return_true_instance.skill.name == "ReturnTrue"
     assert not return_true_instance.bindings
 
     assert forty_four_instance.skill.name == "AddOne"
     expected_44 = forty_four_instance.bindings.get("value")
-    assert expected_44 == 44
+    assert expected_44 == "44"
 
     assert negative_five_instance.skill.name == "AddOne"
     expected_neg_5 = negative_five_instance.bindings.get("value")
-    assert expected_neg_5 == -5
+    assert expected_neg_5 == "-5"
