3565d729087288f2f3f17b3a40eee94a474dc2f8
Benned Hedegaard <benned1@gmail.com>
2025-10-22 16:17:50 -0400

    Start loop of calling thread class

--- src/robotics_utils/ros/call_loop_thread.py
+++ src/robotics_utils/ros/call_loop_thread.py
@@ -5,21 +5,21 @@ from typing import Callable
 
 import rospy
 
-from robotics_utils.ros import TransformManager
-
 
 class CallLoopThread:
     """A class that continually calls a given function in an endless loop."""
 
-    def __init__(self, func: Callable) -> None:
+    def __init__(self, func: Callable, loop_hz: float = 10.0) -> None:
         """Initialize a thread to call the given function in a loop."""
         self._function = func
+        self._loop_hz = loop_hz
         self._thread = threading.Thread(target=self._loop, daemon=True)
+        self._thread.start()
 
     def _loop(self) -> None:
         """Continually call the stored function."""
         try:
-            rate_hz = rospy.Rate(TransformManager.LOOP_HZ)
+            rate_hz = rospy.Rate(self._loop_hz)
             while not rospy.is_shutdown():
                 self._function()
                 rate_hz.sleep()
