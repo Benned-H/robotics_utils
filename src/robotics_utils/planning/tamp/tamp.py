879de58a729416ac6ec3840de9eba41f096a3356
Benned <benned1@gmail.com>
2026-02-13 15:47:41 -0500

    Clarify states/poses/abstract states

--- src/robotics_utils/planning/tamp/tamp.py
+++ src/robotics_utils/planning/tamp/tamp.py
@@ -7,8 +7,15 @@ Reference:
     Automation (ICRA), May 2014, pp. 639–646. doi: 10.1109/ICRA.2014.6906922.
 """
 
-State = bool  # TODO: Implement real type
-Pose = bool  # TODO: Implement real type
+from typing import TypeVar
+
+StateT = TypeVar("StateT")
+"""Type variable representing a low-level environment state."""
+
+AbstractStateT = TypeVar("AbstractStateT")
+"""Type variable representing an abstract symbolic state."""
+
+
 ErrFreeMode = bool  # TODO: Should be enum
 PartialTrajMode = bool  # TODO: Should be enum
 TaskPlan = bool  # TODO: Implement real type
@@ -16,46 +23,50 @@ Trajectory = bool  # TODO: Implement real type
 Mode = bool  # TODO: Implement real type (enum or bool?)
 
 
-def tamp(state: State, initial_pose: Pose) -> None:
+def tamp(initial_ll_state: StateT, initial_hl_state: AbstractStateT) -> None:
     """Implement Algorithm 1 of Srivastava et al. (ICRA 2014)."""
     if task_plan is None:
-        task_plan = task_planner.plan(state)
+        task_plan = task_planner.plan(initial_hl_state)
         step = 1
         partial_traj = None
-        pose1 = initial_pose
+        s1 = initial_ll_state
 
     while not resource_limit_reached():
-        refine_result = try_refine(pose1, task_plan, step, partial_traj, ErrFreeMode)
+        refine_result = try_refine(s1, task_plan, step, partial_traj, ErrFreeMode)
         if refine_result is not None:
             return refine_result
 
         while True:
-            partial_traj, pose2, fail_step, fail_cause = try_refine(
-                pose1,
+            partial_traj, s2, fail_step, fail_cause = try_refine(
+                s1,
                 task_plan,
                 step,
                 partial_traj,
                 PartialTrajMode,
             )
-            state = state.update(fail_cause, fail_step)
-            new_plan = task_planner.plan(state)
+            hl_state = hl_state.update(fail_cause, fail_step)
+            new_plan = task_planner.plan(hl_state)
             if new_plan is not None:
                 task_plan = task_plan[:fail_step] + new_plan
-                pose1 = pose2
+                s1 = s2
                 step = fail_step
 
-            if new_plan is not None or traj_count > max_traj_count:
+            if new_plan is not None or traj_count >= max_traj_count:
                 break
 
-        if traj_count > max_traj_count:
-            # Clear all learned facts from the initial state
-            # Reset pose generators with new random seed
-            # Reset step, partial_traj, pose1 to initial values
-            pass
+        if traj_count >= max_traj_count:
+            # TODO: Clear all learned facts from the initial state
+            hl_state = initial_hl_state
+
+            # TODO: Reset pose generators with new random seed
+
+            step = 1
+            partial_traj = None
+            s1 = initial_ll_state
 
 
 def try_refine(
-    initial_pose: Pose,
+    initial_state: StateT,
     task_plan: TaskPlan,
     step: int,
     traj_prefix: Trajectory,
@@ -67,23 +78,23 @@ def try_refine(
         index = step - 1
         traj = traj_prefix
         # TODO: Initialize pose generators
-        pose1 = initial_pose
+        s1 = initial_state
     while step - 1 <= index <= len(task_plan):
         action = task_plan[index]
         next_action = task_plan[index + 1]
-        pose2 = action.pose_generator.next()
-        if pose2 is None:
+        s2 = action.pose_generator.next()
+        if s2 is None:
             next_action.pose_generator.reset()
-            pose1 = action.pose_generator.next()
+            s1 = action.pose_generator.next()
             index -= 1
             traj = traj.delete_suffix_for(action)
             continue
-        plan = motion_planner.plan(pose1, pose2)
+        plan = motion_planner.plan(s1, s2)
         if plan is not None:
             if index == len(task_plan) + 1:
                 return traj
             traj = traj + plan.computed_path
             index += 1
-            pose1 = pose2
+            s1 = s2
         elif mode == PartialTrajMode:
-            return (pose1, traj, index + 1, plan.mp_errors)
+            return (s1, traj, index + 1, plan.mp_errors)
